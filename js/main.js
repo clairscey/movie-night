cb25168be2808cf1e1b2d0bfa843f23f8d8dd70f
Chris <clairscey@gmail.com>
2022-04-10 21:23:11 -0500

    update js to play all trailers

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-
-// let userDrink = document.querySelector('input').value
-
-// console.log(userDrink)
-
-document.querySelector('button').addEventListener('click', searchMovie)
-
-document.querySelector('#movie1').addEventListener('click', displayTrailer)
 
 let movie1;
 let movie2;
 let movie3;
+
+document.querySelector('button').addEventListener('click', searchMovie)
+
+document.querySelector('#movie1').addEventListener('click', displayTrailer1)
+document.querySelector('#movie2').addEventListener('click', displayTrailer2)
+document.querySelector('#movie3').addEventListener('click', displayTrailer3)
+
+
 
 function searchMovie() {
     let userSearch = document.querySelector('input').value;
@@ -22,21 +21,46 @@ function searchMovie() {
       console.log(data)
       document.querySelector('#movie1').src = data.results[0].image
       movie1 = data.results[0].id
-      document.querySelector('#secondChoice').innerHTML = `<img id='movie2' src=${data.results[1].image}>`
-      document.querySelector('#thirdChoice').innerHTML = `<img id='movie3' src=${data.results[2].image}>`
+      document.querySelector('#movie2').src = data.results[1].image
+      movie2 = data.results[1].id
+      document.querySelector('#movie3').src = data.results[2].image
+      movie3 = data.results[2].id
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 }
 
-function displayTrailer() {
+function displayTrailer1() {
   fetch(`https://imdb-api.com/en/API/YouTubeTrailer/k_oc1edjb6/${movie1}`)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
     console.log(data)
     let videoUrlClean = 'https://www.youtube-nocookie.com/embed/' + data.videoUrl.slice(32);
-    // document.querySelector('#trailerContainer').classList.toggle('hidden');
+    document.querySelector('#trailerContainer').src = videoUrlClean
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+}
+function displayTrailer2() {
+  fetch(`https://imdb-api.com/en/API/YouTubeTrailer/k_oc1edjb6/${movie2}`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    let videoUrlClean = 'https://www.youtube-nocookie.com/embed/' + data.videoUrl.slice(32);
+    document.querySelector('#trailerContainer').src = videoUrlClean
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+}
+function displayTrailer3() {
+  fetch(`https://imdb-api.com/en/API/YouTubeTrailer/k_oc1edjb6/${movie3}`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    let videoUrlClean = 'https://www.youtube-nocookie.com/embed/' + data.videoUrl.slice(32);
     document.querySelector('#trailerContainer').src = videoUrlClean
   })
   .catch(err => {
